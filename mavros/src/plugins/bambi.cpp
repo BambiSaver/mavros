fdea82af284f087b0f50d96b5a45200d6e34d77e
florian-world <m@florian.world>
2018-08-07 21:30:26 +0200

    Updated log messages in bambi plugin and some refactoring

--- mavros/src/plugins/bambi.cpp
+++ mavros/src/plugins/bambi.cpp
@@ -19,6 +19,8 @@
 #include <mavros/mavros_plugin.h>
 #include <mavros_msgs/BambiMissionTrigger.h>
 
+#include <ros/types.h>
+
 #include <eigen_conversions/eigen_msg.h>
 
 #include <geometry_msgs/PoseStamped.h>
@@ -45,7 +47,7 @@ class BambiPlugin : public plugin::PluginBase {
     PluginBase::initialize(uas_);
     ROS_INFO("!!!!!!!!!!!!BAMBI INITIALIZATION!!!!!!!!");
     mission_trigger = bambi_nh.advertise<mavros_msgs::BambiMissionTrigger>(
-        "missionTrigger", 2, false);
+        "missiontrigger", 2, false);
   }
 
   Subscriptions get_subscriptions() {
@@ -61,10 +63,8 @@ class BambiPlugin : public plugin::PluginBase {
 
   void handle_mission_trigger(const mavlink::mavlink_message_t *msg,
                               mavlink::common::msg::COMMAND_LONG &command) {
-    ROS_INFO("BAMBI GOT A MESSAGE!!!!!!!!");
-    ROS_INFO("TARGET (%d, %d) COMMAND_ID: %d", command.target_system,
-             command.target_component, command.command);
-
+//    ROS_INFO("BAMBI GOT A MESSAGE!!!!!!!!");
+    
     // only accept 0,1 and 240
     // TODO: change to MYID somehow
     if (command.target_system > 1 || command.target_component != 240) {
@@ -72,6 +72,10 @@ class BambiPlugin : public plugin::PluginBase {
     }
 
     using mavlink::common::MAV_CMD;
+    
+    ROS_INFO("TARGET (%d, %d) COMMAND_ID: %d", command.target_system,
+             command.target_component, command.command);
+    
 
 
     auto commandType = static_cast<MAV_CMD>(command.command);
@@ -79,12 +83,23 @@ class BambiPlugin : public plugin::PluginBase {
     switch (commandType) {
       // should be MAV_CMD::BAMBI....
       case MAV_CMD::BAMBI_START_STOP_MISSION:
+      
+        // TODO: decodify from mavlink (msg) command the altitude, target position etc.
+      
+        double lat = 46.452951;
+        double lon = 11.492170;
+        uint8_t altitude = 50;
+        bool startStop = true;
+        
+      
+        ROS_INFO("BAMBI PLUGIN in mavros got a mission trigger to %s the mission", startStop ? "start" : "stop");
         boost::shared_ptr<mavros_msgs::BambiMissionTrigger> mission_trigger_msg =
             boost::make_shared<mavros_msgs::BambiMissionTrigger>();
 
-        mission_trigger_msg->startStop = true;
-        //mission_trigger->missionBasePoint 3xdouble
-        mission_trigger_msg->startAltitutdeOverGround = 30;
+        mission_trigger_msg->startStop = startStop;
+        mission_trigger_msg->missionBasePoint.latitude = lat;
+        mission_trigger_msg->missionBasePoint.longitude = lon;
+        mission_trigger_msg->startAltitutdeOverGround = altitude;
 
         mission_trigger.publish(mission_trigger_msg);
         break;
